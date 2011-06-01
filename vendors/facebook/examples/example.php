e9acc4653ab56c27217ab890a25484c7030ba6d5
Nick Baker <nick@webtechnick.com>
2011-06-01 11:36:26 -0600

    Reverting back to 2.5.0, 3.0.1 PHP SDK is not ready for FBXML implimentation. Converting to a branch for testing.

--- vendors/facebook/examples/example.php
+++ vendors/facebook/examples/example.php
@@ -1,19 +1,4 @@
 <?php
-/**
- * Copyright 2011 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 
 require '../src/facebook.php';
 
@@ -21,29 +6,31 @@ require '../src/facebook.php';
 $facebook = new Facebook(array(
   'appId'  => '117743971608120',
   'secret' => '943716006e74d9b9283d4d5d8ab93204',
+  'cookie' => true,
 ));
 
-// Get User ID
-$user = $facebook->getUser();
-
-// We may or may not have this data based on whether the user is logged in.
+// We may or may not have this data based on a $_GET or $_COOKIE based session.
 //
-// If we have a $user id here, it means we know the user is logged into
-// Facebook, but we don't know if the access token is valid. An access
-// token is invalid if the user logged out of Facebook.
+// If we get a session here, it means we found a correctly signed session using
+// the Application Secret only Facebook and the Application know. We dont know
+// if it is still valid until we make an API call using the session. A session
+// can become invalid if it has already expired (should not be getting the
+// session back in this case) or if the user logged out of Facebook.
+$session = $facebook->getSession();
 
-if ($user) {
+$me = null;
+// Session based API call.
+if ($session) {
   try {
-    // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
+    $uid = $facebook->getUser();
+    $me = $facebook->api('/me');
   } catch (FacebookApiException $e) {
     error_log($e);
-    $user = null;
   }
 }
 
-// Login or logout url will be needed depending on current user state.
-if ($user) {
+// login or logout url will be needed depending on current user state.
+if ($me) {
   $logoutUrl = $facebook->getLogoutUrl();
 } else {
   $loginUrl = $facebook->getLoginUrl();
@@ -71,31 +58,69 @@ $naitik = $facebook->api('/naitik');
     </style>
   </head>
   <body>
-    <h1>php-sdk</h1>
+    <!--
+      We use the JS SDK to provide a richer user experience. For more info,
+      look here: http://github.com/facebook/connect-js
+    -->
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId   : '<?php echo $facebook->getAppId(); ?>',
+          session : <?php echo json_encode($session); ?>, // don't refetch the session when PHP already has it
+          status  : true, // check login status
+          cookie  : true, // enable cookies to allow the server to access the session
+          xfbml   : true // parse XFBML
+        });
 
-    <?php if ($user): ?>
-      <a href="<?php echo $logoutUrl; ?>">Logout</a>
+        // whenever the user logs in, we refresh the page
+        FB.Event.subscribe('auth.login', function() {
+          window.location.reload();
+        });
+      };
+
+      (function() {
+        var e = document.createElement('script');
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+        e.async = true;
+        document.getElementById('fb-root').appendChild(e);
+      }());
+    </script>
+
+
+    <h1><a href="example.php">php-sdk</a></h1>
+
+    <?php if ($me): ?>
+    <a href="<?php echo $logoutUrl; ?>">
+      <img src="http://static.ak.fbcdn.net/rsrc.php/z2Y31/hash/cxrz4k7j.gif">
+    </a>
     <?php else: ?>
-      <div>
-        Login using OAuth 2.0 handled by the PHP SDK:
-        <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
-      </div>
+    <div>
+      Using JavaScript &amp; XFBML: <fb:login-button></fb:login-button>
+    </div>
+    <div>
+      Without using JavaScript &amp; XFBML:
+      <a href="<?php echo $loginUrl; ?>">
+        <img src="http://static.ak.fbcdn.net/rsrc.php/zB6N8/hash/4li2k73z.gif">
+      </a>
+    </div>
     <?php endif ?>
 
-    <h3>PHP Session</h3>
-    <pre><?php print_r($_SESSION); ?></pre>
+    <h3>Session</h3>
+    <?php if ($me): ?>
+    <pre><?php print_r($session); ?></pre>
 
-    <?php if ($user): ?>
-      <h3>You</h3>
-      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+    <h3>You</h3>
+    <img src="https://graph.facebook.com/<?php echo $uid; ?>/picture">
+    <?php echo $me['name']; ?>
 
-      <h3>Your User Object (/me)</h3>
-      <pre><?php print_r($user_profile); ?></pre>
+    <h3>Your User Object</h3>
+    <pre><?php print_r($me); ?></pre>
     <?php else: ?>
-      <strong><em>You are not Connected.</em></strong>
+    <strong><em>You are not Connected.</em></strong>
     <?php endif ?>
 
-    <h3>Public profile of Naitik</h3>
+    <h3>Naitik</h3>
     <img src="https://graph.facebook.com/naitik/picture">
     <?php echo $naitik['name']; ?>
   </body>
--- vendors/facebook/readme.md
+++ vendors/facebook/readme.md
@@ -1,5 +1,5 @@
-Facebook PHP SDK (v.3.0.0)
-==========================
+Facebook PHP SDK
+================
 
 The [Facebook Platform](http://developers.facebook.com/) is
 a set of APIs that make your application more social. Read more about
@@ -18,34 +18,30 @@ Usage
 The [examples][examples] are a good place to start. The minimal you'll need to
 have is:
 
+    <?php
+
     require './facebook.php';
 
     $facebook = new Facebook(array(
-      'appId'  => 'YOUR_APP_ID',
-      'secret' => 'YOUR_APP_SECRET',
+      'appId'  => 'YOUR APP ID',
+      'secret' => 'YOUR API SECRET',
+      'cookie' => true, // enable optional cookie support
     ));
-
-    // Get User ID
-    $user = $facebook->getUser();
 
 To make [API][API] calls:
 
-    if ($user) {
-      try {
-        // Proceed knowing you have a logged in user who's authenticated.
-        $user_profile = $facebook->api('/me');
-      } catch (FacebookApiException $e) {
-        error_log($e);
-        $user = null;
-      }
+    try {
+      $me = $facebook->api('/me');
+    } catch (FacebookApiException $e) {
+      error_log($e);
     }
 
-Login or logout url will be needed depending on current user state.
+Logged in vs Logged out:
 
-    if ($user) {
-      $logoutUrl = $facebook->getLogoutUrl();
+    if ($facebook->getSession()) {
+      echo '<a href="' . $facebook->getLogoutUrl() . '">Logout</a>';
     } else {
-      $loginUrl = $facebook->getLoginUrl();
+      echo '<a href="' . $facebook->getLoginUrl() . '">Login</a>';
     }
 
 [examples]: http://github.com/facebook/php-sdk/blob/master/examples/example.php
@@ -55,9 +51,10 @@ Login or logout url will be needed depending on current user state.
 Feedback
 --------
 
-File bugs or other issues [here].
+We are relying on the [GitHub issues tracker][issues] linked from above for
+feedback. File bugs or other issues [here][issues].
 
-[here]: http://bugs.developers.facebook.net/
+[issues]: http://github.com/facebook/php-sdk/issues
 
 
 
